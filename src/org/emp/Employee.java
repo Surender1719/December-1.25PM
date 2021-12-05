7d95ab639805ad3c2887c8b78b28c54ac663c50a
Surender1719 <ncsuren@gmail.com>
2021-12-05 13:14:44 +0530

    Done

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,40 @@
+package org.emp;
+
+public class Employee {
+	
+	public static void main(String[] args) {
+		int empId=123456;//6
+		System.out.println("Employee Id:"+empId);
+		
+		String name="surender";
+		System.out.println("Employee Name:"+name);
+		
+		String email="tcs@com";
+		System.out.println("Employee Email:"+email);
+		
+		long phoneno=9840975548l;
+		System.out.println("Employee Phoneno:"+phoneno);
+		
+		float sal=23254.232546789f;
+		System.out.println("Employee Salary:"+sal);
+		
+		char gen='m';
+		System.out.println("Employee Gender:"+gen);
+		
+		String city="chennai";
+		System.out.println("Employee City:"+city);
+		
+		boolean b=true;
+		System.out.println(b);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
